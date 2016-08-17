16842ff3aa708fac148a305329656a470406a70a
cyyuen <colsy2@gmail.com>
2016-08-17 19:36:24 +0800

    improve installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+git submodule init
+git submodule update
+
 current_path=$(pwd)
 
-mv ~/.bash_profile{,.old}
+mv ~/.bash_profile{,.old} 2> /dev/null
+rm -f ~/.bash-git-prompt
 
 ln -s "$current_path"/.bash_profile ~/.bash_profile
 ln -s "$current_path"/bash-git-prompt ~/.bash-git-prompt
